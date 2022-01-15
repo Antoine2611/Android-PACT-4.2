53e26f899fd0afe706a4d76a98aa812b740ab143
Antoine Andurao <antoine.andurao@telecom-paris.fr>
2022-01-15 17:03:03 +0100

    Test on MarkerListener

--- app/src/main/java/fr/enst/pact42/sportbox/MapsActivity.java
+++ app/src/main/java/fr/enst/pact42/sportbox/MapsActivity.java
@@ -8,12 +8,14 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import fr.enst.pact42.sportbox.databinding.ActivityMapsBinding;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
 
     private GoogleMap mMap;
     private ActivityMapsBinding binding;
@@ -48,5 +50,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Paris"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+        LatLng test = new LatLng(-35, 150);
+        mMap.addMarker(new MarkerOptions().position(test).title("Marker in Paris"));
+        return false;
     }
 }
