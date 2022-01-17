e3513bc96ec4102a7c1571c7ec1cb19827ef526a
DanaeX <antoine.andurao@yahoo.fr>
2022-01-17 02:59:31 +0100

    Mode nuit sur la carte, retrait des marqueurs qui ne sont pas de SportBox et mise en place de deux marqueurs custom réagissant au click

--- app/src/main/java/fr/enst/pact42/sportbox/MapsActivity.java
+++ app/src/main/java/fr/enst/pact42/sportbox/MapsActivity.java
@@ -15,9 +15,13 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.Random;
 
 import fr.enst.pact42.sportbox.databinding.ActivityMapsBinding;
 
@@ -52,18 +56,26 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Paris"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap = googleMap;
+        mMap.setOnMarkerClickListener(this);
+
+        boolean success = googleMap.setMapStyle(new MapStyleOptions(getResources()
+                .getString(R.string.style_json)));
+
+        mMap.addMarker(new MarkerOptions().position(new LatLng(48.71201566810882, 2.2149531159803737)).title("Gymnase de l'ENSTA").icon(BitmapDescriptorFactory.defaultMarker(0)));
+
+        LatLng tetechLatLng = new LatLng(48.71256936082508, 2.200860956792873);
+        mMap.addMarker(new MarkerOptions().position(tetechLatLng).title("Télécom Paris").icon(BitmapDescriptorFactory.defaultMarker(150)));
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(tetechLatLng, mMap.getMaxZoomLevel()-5));
     }
 
     @Override
     public boolean onMarkerClick(Marker marker) {
-        LatLng test = new LatLng(-35, 150);
-        mMap.addMarker(new MarkerOptions().position(test).title("Marker in Paris"));
+        Random r = new Random();
+        int colorHue = r.nextInt(360);
+        marker.setIcon(BitmapDescriptorFactory.defaultMarker(colorHue));
         return false;
     }
 
