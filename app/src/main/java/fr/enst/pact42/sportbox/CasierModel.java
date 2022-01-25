fee553489252837b3f17e6cb929902b6a3006aff
Kelthoum KERBOUA <kelthoum.kerboua@gmail.com>
2022-01-25 10:19:13 +0100

    Textes, Bouttons, Modèle sur activité casier accessible depuis carte

--- app/src/main/java/fr/enst/pact42/sportbox/CasierModel.java
+++ app/src/main/java/fr/enst/pact42/sportbox/CasierModel.java
@@ -5,6 +5,7 @@ public class CasierModel {
     private String typeMaterial;
     private String condition;
     private String localisation;
+    private boolean access;
 
     public CasierModel(String typeMaterial, String condition, String localisation){
         this.typeMaterial=typeMaterial;
@@ -21,5 +22,13 @@ public class CasierModel {
 
     public String getLocalisation() {
         return localisation;
+    }
+
+    public boolean getAccess() {
+        return access;
+    }
+
+    public void setAccess(boolean access) {
+        this.access = access;
     }
 }
--- app/src/main/java/fr/enst/pact42/sportbox/InfoCasierActivity.java
+++ app/src/main/java/fr/enst/pact42/sportbox/InfoCasierActivity.java
@@ -3,6 +3,7 @@ package fr.enst.pact42.sportbox;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -19,6 +20,30 @@ public class InfoCasierActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info_casier);
+    typeMaterial = findViewById(R.id.type_material);
+    condition=findViewById(R.id.condition);
+    localisation= findViewById(R.id.localisation);
+    unlock=findViewById(R.id.unlock);
+    report=findViewById(R.id.report);
+    unlock.setEnabled(casier.getAccess());
+    report.setEnabled(true);
+    typeMaterial.setText("Type de matériel:   "+ casier.getTypeMaterial());
+    condition.setText("Etat:   "+ casier.getCondition());
+    localisation.setText("Lieu:   "+ casier.getLocalisation());
+
+    unlock.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            // The user just clicked on unlock button
+        }
+    });
+
+    report.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            // The user just clicked on report button
+        }
+    });
 
 
     }
