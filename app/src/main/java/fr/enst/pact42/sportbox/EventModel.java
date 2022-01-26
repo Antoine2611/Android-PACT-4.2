f43bb1b79cf0c4b426788aa051410010f4e1ecb4
Kelthoum KERBOUA <kelthoum.kerboua@gmail.com>
2022-01-26 02:19:55 +0100

    Changement sur les activités et fichiers xml informations casier et évenements

---
+++ app/src/main/java/fr/enst/pact42/sportbox/EventModel.java
@@ -0,0 +1,45 @@
+package fr.enst.pact42.sportbox;
+
+public class EventModel {
+
+    private String typeEvent;
+    private String localisation;
+    private String level;
+    private int nbParticipations;
+    private int limitParticipations;
+
+    public EventModel(String typeEvent, String localisation, String level, int participations){
+        this.typeEvent=typeEvent;
+        this.localisation=localisation;
+        this.level=level;
+        nbParticipations=0;
+        limitParticipations=participations;
+    }
+    public boolean isAvailable(){
+        return nbParticipations!=limitParticipations;
+    }
+
+    public String getTypeEvent() {
+        return typeEvent;
+    }
+
+    public int getNbParticipations() {
+        return nbParticipations;
+    }
+
+    public void setNbParticipations(int nbParticipations) {
+        this.nbParticipations = nbParticipations;
+    }
+
+    public String getLocalisation() {
+        return localisation;
+    }
+
+    public int getLimitParticipations() {
+        return limitParticipations;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+}
--- app/src/main/java/fr/enst/pact42/sportbox/InfoEventActivity.java
+++ app/src/main/java/fr/enst/pact42/sportbox/InfoEventActivity.java
@@ -3,12 +3,43 @@ package fr.enst.pact42.sportbox;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class InfoEventActivity extends AppCompatActivity {
+
+    private TextView typeEvents;
+    private TextView localisation;
+    private TextView participations;
+    private TextView level;
+    private Button participate;
+    private EventModel event =new EventModel("Cours de Yoga", "Télécom Paris", "débutant", 15);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info_event);
+
+        typeEvents = findViewById(R.id.type_events);
+        participations=findViewById(R.id.participations);
+        localisation= findViewById(R.id.localisation_event);
+        level= findViewById(R.id.level);
+        participate=findViewById(R.id.participate);
+
+        participate.setEnabled(event.isAvailable());
+
+        typeEvents.setText("Type d'événement: "+ event.getTypeEvent());
+        localisation.setText("Lieu: "+ event.getLocalisation());
+        level.setText("Niveau: "+ event.getLevel());
+        participations.setText("Participations: "+ String.valueOf(event.getNbParticipations()) + " / " + String.valueOf(event.getLimitParticipations()));
+
+        participate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //add one participant
+
+            }
+        });
     }
 }
