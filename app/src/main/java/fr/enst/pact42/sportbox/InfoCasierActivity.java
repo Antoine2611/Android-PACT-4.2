dfb5be5c81350e5c5e090d9ad8e2ce41680d6f04
Kelthoum KERBOUA <kelthoum.kerboua@gmail.com>
2022-03-23 13:04:25 +0100

    Update spinner InfoCasierActivity

--- app/src/main/java/fr/enst/pact42/sportbox/InfoCasierActivity.java
+++ app/src/main/java/fr/enst/pact42/sportbox/InfoCasierActivity.java
@@ -2,7 +2,9 @@ package fr.enst.pact42.sportbox;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,28 +13,31 @@ import java.util.ArrayList;
 
 public class InfoCasierActivity extends AppCompatActivity {
 
+    private Spinner spinner;
     private TextView typeMaterial;
     private TextView condition;
     private TextView localisation;
     private Button unlock;
     private Button report;
     private ArrayList<CasierModel> casiers;
-    private CasierModel casier = new CasierModel ("Ballon de foot", "Correct", "Télécom Paris", true);
+    private CasierModel casier;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info_casier);
+        casier = new CasierModel ("Ballon de foot", "Correct", "Télécom Paris", true);
+        casiers = new ArrayList<CasierModel>();
+        casiers.add(casier);
+        casiers.add(new CasierModel ("Ballon de Basket", "Mauvais", "Télécom Paris", true));
+
     typeMaterial = findViewById(R.id.type_material);
     condition=findViewById(R.id.condition);
     localisation= findViewById(R.id.localisation);
     unlock=findViewById(R.id.unlock);
     report=findViewById(R.id.report);
-    unlock.setEnabled(casier.getAccess());
-    report.setEnabled(true);
-    typeMaterial.setText("Type de matériel: "+ casier.getTypeMaterial());
-    condition.setText("Etat: "+ casier.getCondition());
-    localisation.setText("Lieu: "+ casier.getLocalisation());
+    spinner=findViewById(R.id.spinner);
+    updateActivity();
 
     unlock.setOnClickListener(new View.OnClickListener() {
         @Override
@@ -48,6 +53,23 @@ public class InfoCasierActivity extends AppCompatActivity {
         }
     });
 
+    ArrayList<String> options= new ArrayList<String>();
 
+    for (CasierModel c: casiers){
+        options.add(c.getTypeMaterial());
     }
+    ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item,options);
+    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+    spinner.setAdapter(adapter);
+    }
+
+    public void updateActivity(){
+        unlock.setEnabled(casier.getAccess());
+        report.setEnabled(true);
+        typeMaterial.setText("Type de matériel: "+ casier.getTypeMaterial());
+        condition.setText("Etat: "+ casier.getCondition());
+        localisation.setText("Lieu: "+ casier.getLocalisation());
+    }
+
+
 }
