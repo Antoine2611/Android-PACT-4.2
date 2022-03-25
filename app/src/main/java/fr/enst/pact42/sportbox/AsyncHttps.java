aba97247086bb9a9ec2f1572c35da9b1f098abab
Kelthoum KERBOUA <kelthoum.kerboua@gmail.com>
2022-03-25 09:37:36 +0100

    Création AsyncTask pour requête https

---
+++ app/src/main/java/fr/enst/pact42/sportbox/AsyncHttps.java
@@ -0,0 +1,12 @@
+package fr.enst.pact42.sportbox;
+
+import android.os.AsyncTask;
+
+import org.json.JSONObject;
+
+public class AsyncHttps extends AsyncTask<String,Integer, JSONObject> {
+    @Override
+    protected JSONObject doInBackground(String... strings) {
+        return null;
+    }
+}
